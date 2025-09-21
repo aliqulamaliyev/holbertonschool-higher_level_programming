349f4a95491891f33f40584b1f65a8a164ab84f4
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-09-21 14:09:34 +0000

    completes al python tasks

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-"""Square Class
-
-A Square Class
-
-"""
+"""Module for creating Square class"""
 
 
 class Square:
-
+    """A class representing Square"""
     def __init__(self, size):
-        """__init__
-
-        The __init__ method initializes the size value
-        of the square.
-
-        Attributes:
-            size (int): The size of the square.
-
-        """
+        """Instantiation with a given size"""
         self.__size = size
