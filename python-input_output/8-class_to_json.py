19fce703977edfed8c693b6d2d7b0d6135e7f936
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-12-05 07:24:42 +0000

    complete all tasks

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description with simple data structure
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of an object for JSON serialization.
+
+    Args:
+        obj: Instance of a class.
+
+    Returns:
+        dict: Dictionary of the instance attributes.
+    """
+    return obj.__dict__.copy()
